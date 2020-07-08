c6db9e2c86a66d2c40924e4547f3e061bf4fe5cb
chopcheez <phillip109@gmail.com>
2020-07-08 11:22:35 -0400

    Caesar cipher works!

---
+++ README.md
@@ -0,0 +1,2 @@
+Created a caesar cipher in ruby that takes a message from the user to encode by shifting each letter a given number of characters.
+Case and punctuation are kept the same.
--- caesar-cipher.rb
+++ caesar-cipher.rb
@@ -1,10 +1,26 @@
-#Ask user for message to encrypt
-#Ask user for shift number
-#Create variables to store message and shift number
-#Create method to shift each letter in message by given number
+puts "Tell me a secret" #Ask user for message to encrypt
+message = gets.chomp #Store message in variable
+puts "Shift by:" #Ask user for shift number
+shift = gets.chomp.to_i #Store shift number in variable
 
-puts "Tell me a secret"
-message = gets.chomp
-puts "Shift by:"
-shift = gets.chomp.to_i
+def caesar_cipher(str, num) #Create method to shift each letter in message by given number
+    dict = ("a".."z").to_a
+    dict_cap = ("A".."Z").to_a
+    i = 0
+    caesar = ""
+    while i < str.length do
+        if str[i].match(/\W/)
+            caesar << str[i]
+            i += 1
+        elsif str[i] === str[i].capitalize
+            caesar << dict_cap[(dict_cap.index(str[i]) + num) % 26]
+            i += 1
+        else
+            caesar << dict[(dict.index(str[i]) + num) % 26]
+            i += 1
+        end
+    end
+    puts caesar
+end
 
+caesar_cipher(message, shift)
