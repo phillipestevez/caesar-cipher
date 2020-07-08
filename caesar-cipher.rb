a0265ff6ef246bbbeb23bf6bf27755d47dea9b6b
chopcheez <phillip109@gmail.com>
2020-07-08 09:58:43 -0400

    Initial commit

---
+++ caesar-cipher.rb
@@ -0,0 +1,10 @@
+#Ask user for message to encrypt
+#Ask user for shift number
+#Create variables to store message and shift number
+#Create method to shift each letter in message by given number
+
+puts "Tell me a secret"
+message = gets.chomp
+puts "Shift by:"
+shift = gets.chomp.to_i
+
